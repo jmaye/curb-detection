381f1ea2143468f2d53726c522f53f3832b5f147
Jerome Maye <jerome.maye@mavt.ethz.ch>
2011-06-22 10:36:20 +0000

    Update doc

---
+++ code/src/lib/Doc.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright (C) 2011 by Jerome Maye                                          *
+ * jerome.maye@gmail.com                                                      *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the Lesser GNU General Public License as published by*
+ * the Free Software Foundation; either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * Lesser GNU General Public License for more details.                        *
+ *                                                                            *
+ * You should have received a copy of the Lesser GNU General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ ******************************************************************************/
+
+/** \file Doc.h
+    \brief Contains main page for Doxygen documentation
+  */
+
+/** \mainpage Reference manual for curb-detection - A free/open source C++ library for curb detection in 3D point clouds
+    \author Jerome Maye <jerome.maye@gmail.com>
+    \version 0.1.100
+    \date June 22, 2011
+  */
+
+/** \page license License
+    <hr size="1">
+    \section license-license License
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    Lesser GNU General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+  */
